2fc35e67a95a65990273fa410954fd1417ea0a81
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-23 15:58:51 +0200

    feat(task6): extend factory method with product C

---
+++ task6/FactoryMethodHw/Program.cs
@@ -0,0 +1,52 @@
+﻿﻿using System;
+namespace FactoryMethodExample
+{
+    //абстрактний клас творця, який має абстрактний метод FactoryMethod, що приймає тип продукта
+    public abstract class Creator
+    {
+        public abstract Product FactoryMethod(int type);
+    }
+
+    public class ConcreteCreator : Creator
+    {
+        public override Product FactoryMethod(int type)
+        {
+            switch (type)
+            {
+                //повертає об'єкт A, якщо type==1
+                case 1: return new ConcreteProductA();
+                //повертає об'єкт B, якщо type==2  
+                case 2: return new ConcreteProductB();
+                // added: повертає об'єкт C, якщо type==3
+                case 3: return new ConcreteProductC();
+                default: throw new ArgumentException("Invalid type.", "type");
+            }
+        }
+    }
+
+    public abstract class Product { } //абстрактний клас продукт
+
+    //конкретні продукти з різною реалізацією
+    public class ConcreteProductA : Product { }
+
+    public class ConcreteProductB : Product { }
+
+    // added
+    public class ConcreteProductC : Product { }
+
+    class MainApp
+    {
+        static void Main()
+        {       //створюємо творця
+            Creator creator = new ConcreteCreator();
+            // changed for 3
+            for (int i = 1; i <= 3; i++)
+            {
+                //створюємо продукти з різними типами
+                var product = creator.FactoryMethod(i);
+                Console.WriteLine("Where id = {0}, Created {1} ", i, product.GetType());
+            }
+            Console.ReadKey();
+        }
+    }
+}
