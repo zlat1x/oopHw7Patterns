fa88d01219d96713ada8c1f4ed68404e412eb429
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-23 16:48:29 +0200

    feat(task6): add Facade MethodC with SubSystemFive

---
+++ task6/FacadeHw/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Examples
+{
+    // Mainapp test application 
+    class MainApp
+    {
+        public static void Main()
+        {
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+            facade.MethodC(); // added
+            // Wait for user 
+            Console.Read();
+        }
+    }
+
+
+    // "Subsystem ClassA" 
+    class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine(" SubSystemOne Method");
+        }
+    }
+
+    // Subsystem ClassB" 
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine(" SubSystemTwo Method");
+        }
+    }
+
+
+    // Subsystem ClassC" 
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine(" SubSystemThree Method");
+        }
+    }
+    // Subsystem ClassD" 
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine(" SubSystemFour Method");
+        }
+    }
+
+    // added
+    class SubSystemFive
+    {
+        public void MethodFive()
+        {
+            Console.WriteLine(" SubSystemFive Method");
+        }
+    }
+
+    // "Facade" 
+    class Facade
+    {
+        SubSystemOne one;
+        SubSystemTwo two;
+        SubSystemThree three;
+        SubSystemFour four;
+        SubSystemFive five; // added
+
+        public Facade()
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+            five = new SubSystemFive();  // added
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            two.MethodTwo();
+            three.MethodThree();
+        }
+
+        // added
+        public void MethodC()
+        {
+            Console.WriteLine("\nMethodC() ---- ");
+            one.MethodOne();
+            three.MethodThree();
+            five.MethodFive();
+        }
+    }
+}
